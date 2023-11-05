ec35498609c3aa6a6d4785d496a002060e06925d
BENALI-WIEM <wiem.benali@esprit.tn>
2023-11-05 15:15:16 +0100

    frist commit

---
+++ src/test/java/com/google/ForumEtudiant/ForumEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.ForumEtudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
